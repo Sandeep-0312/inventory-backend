f94e1eec5b1c08954c3c9eb8a2de5a9a3063ac84
Sandeep-0312 <sandyvicky0312@gmail.com>
2025-12-17 17:20:47 +0530

    Use DATABASE_URL for Railway MySQL

--- inventory/settings.py
+++ inventory/settings.py
@@ -106,8 +106,8 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
+        default=os.environ["DATABASE_URL"],
         conn_max_age=600,
-        ssl_require=False
     )
 }
 
