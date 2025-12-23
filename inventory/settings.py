ff4868a77427d0efd9508d3fa7a65f84ea3235ca
Sandeep-0312 <sandyvicky0312@gmail.com>
2025-12-23 11:43:59 +0530

    Allow all hosts to fix Railway proxy host header

--- inventory/settings.py
+++ inventory/settings.py
@@ -18,11 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    ".railway.app",
-]
+ALLOWED_HOSTS = ["*"]
 
 # --------------------------------------------------
 # JWT
