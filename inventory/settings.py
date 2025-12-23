c5c16a764443dfc108ef3dda26ad09f4cc503c14
Sandeep-0312 <sandyvicky0312@gmail.com>
2025-12-23 17:26:34 +0530

    Add missing migration file for accounts app

--- inventory/settings.py
+++ inventory/settings.py
@@ -3,9 +3,10 @@ Django settings for inventory project.
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
-import dj_database_url
+
 
 # --------------------------------------------------
 # Base
@@ -123,6 +124,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
